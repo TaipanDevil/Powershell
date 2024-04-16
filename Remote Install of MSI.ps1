8adfe841cb2e09a838e5b00b35c85c43f7054df6
TaipanDevil <167256198+TaipanDevil@users.noreply.github.com>
2024-04-16 16:59:19 -0600

    Create Remote Install of MSI.ps1

---
+++ Remote Install of MSI.ps1
@@ -0,0 +1,26 @@
+<#
+Title: SingleEndpoint-Install.ps1
+Author: Andrew Zeng
+Creation Date: 2024/03/21 @4PM MDT
+Rev.: 
+Rev. By:
+Rev. Date:
+
+Please Note this script needs to be run in an Admin Powershell Shell to work.
+
+This is a very basic script with no error checking.
+
+#>
+
+$logon = Get-Credential 
+$RemotePC = "ComputerName"
+$DPath = '\\' + $RemotePC +'\c$\Windows\Temp\' #path to where on the remote computer you want to dump the file
+$SPath = '\\Location\Of\Your\MSI\File'
+
+Copy-Item -Path $SPath -Destination $DPath
+
+Invoke-Command -ComputerName $RemotePC -Credential $logon -ScriptBlock {
+          
+     Start-Process -FilePath msiexec.exe -ArgumentList '/i C:\Windows\Temp\ScreenConnectLaptop.msi /quiet /norestart' -Wait #stored here as windows will automatically clean these files
+
+    }
