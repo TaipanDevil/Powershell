0426d78c7e43a7629be34e5084a6cc1995a6706a
TaipanDevil <167256198+TaipanDevil@users.noreply.github.com>
2025-05-05 08:26:07 -0600

    Update Update_User_Properties.ps1

--- Update_User_Properties.ps1
+++ Update_User_Properties.ps1
@@ -10,3 +10,8 @@ import-module activedirectory
 
 $logon = Get-Credential
 $user = Read-Host "Who is the user that you want to update?" 
+if ( Get-ADUser -f { DisplayName -eq $user }){
+  $user = Get-ADUser -f { DisplayName -eq $user } -Properties *
+}
+else ( Write-Host "No user with the name $($user).)
+
